804d42c0baa3ab2259a93e4ab9f13153dadc753d
hamzaamahmood <hamzahmahmoodpak@gmail.com>
2021-01-13 10:33:02 +0500

    First

---
+++ routes/api/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+router.post('/',[
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Minimum 6 characters').isLength({min:6})
+],
+async (req, res) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+     return res.status(400).json({errors: errors.array()});
+   }
+
+   const { name, email, password} = req.body;
+
+   try {
+
+     let user = await User.findOne({email});
+     if(user){
+       res.status(400).json({errors: [{msg: 'User already exists'}]});
+     }
+
+     const avatar = gravatar(email, {
+       s: '200',
+       r: 'pg',
+       d: 'mm'
+     });
+     user = new User({
+       name,
+       email,
+       password,
+       avatar
+     });
+
+     const salt = await bcrypt.genSalt(10);
+     user.password = await brypt.hash(password, salt);
+
+     await user.save();
+
+   } catch (e) {
+      console.log(e.message);
+      res.status(500).send('Server Error');
+   }
+ });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
 
 const app = express();
+connectDB();
+
+app.use(express.json({extended: false}));
 
 app.get('/', (req, res) => res.send('test'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT ||  3000;
 
